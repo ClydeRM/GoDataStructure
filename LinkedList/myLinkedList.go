13b4a1890ba52f82a70c92c0c0f8c6c0c1dbcf07
ClydeRM <clyde.official@icloud.com>
2025-01-01 22:02:49 +0800

    feat(LinkedList): implement linkend list

--- LinkedList/myLinkedList.go
+++ LinkedList/myLinkedList.go
@@ -2,6 +2,22 @@ package LinkedList
 
 import "fmt"
 
+type Node struct {
+	Data int
+	next *Node // next Node mem address
+}
+
+type LinkedList struct {
+	head *Node // head Node pointer
+	length int
+}
+
+func (l *LinkedList) Prepend(n *Node) {
+	second := l.head // tem save current head as second
+	l.head = n // asign new head node
+	l.head.next = second // point second node
+	l.length++
+}
 func PrintLinkedList() {
 	fmt.Println("Hello Linked list.")
 }
--- main.go
+++ main.go
@@ -8,4 +8,15 @@ import (
 func main() {
 	fmt.Println("Hello main.")
 	LinkedList.PrintLinkedList()
+
+	mylist := LinkedList.LinkedList{}
+	nodeA := &LinkedList.Node{Data: 10}
+	nodeB := &LinkedList.Node{Data: 20}
+	nodeC := &LinkedList.Node{Data: 30}
+	nodeD := &LinkedList.Node{Data: 40}
+
+	mylist.Prepend(nodeD)
+	mylist.Prepend(nodeB)
+	mylist.Prepend(nodeC)
+	mylist.Prepend(nodeA)
 }
