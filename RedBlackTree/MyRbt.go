73e0587de353e0ac9d0428fada973d597044a715
ClydeRM <clyde.official@icloud.com>
2025-01-28 17:31:58 +0800

    format(RBT): format code

--- RedBlackTree/MyRbt.go
+++ RedBlackTree/MyRbt.go
@@ -20,13 +20,13 @@ func NewRBT[T constraints.Ordered]() *RBT[T] {
 }
 
 type Tree[T constraints.Ordered] interface {
-    Insert(data T)      // 插入節點
-    Delete(data T)      // 刪除節點
+    Insert(data T)          // 插入節點
+    Delete(data T)          // 刪除節點
     Search(data T) *Node[T] // 查找某個值是否存在
-    Min() (T, bool)     // 找到最小值，若樹為空則返回 false
-    Max() (T, bool)     // 找到最大值，若樹為空則返回 false
-    Size() int          // 返回紅黑樹中節點的數量
-    Height() int        // 返回紅黑樹的高度
-    IsEmpty() bool      // 判斷紅黑樹是否為空
+    Min() (T, bool)         // 找到最小值，若樹為空則返回 false
+    Max() (T, bool)         // 找到最大值，若樹為空則返回 false
+    Size() int              // 返回紅黑樹中節點的數量
+    Height() int            // 返回紅黑樹的高度
+    IsEmpty() bool          // 判斷紅黑樹是否為空
     InOrderTraversal(root *Node[T]) []T
 }
